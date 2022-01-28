ce623a3be74ab23e3a4c9bce9d5b61ffc1eaf757
LenkaVerzichova <verzichova@brilo.cz>
2022-01-28 22:36:03 +0100

    Project init

---
+++ components/layout/header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <p>Hlavička</p>
+    </header>
+  );
+};
+
+export default Header;
---
+++ pages/_app.jsx
@@ -0,0 +1,14 @@
+import '../styles/globals.scss';
+import Layout from './../components/layout/layout';
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
+};
+
+export default MyApp;
