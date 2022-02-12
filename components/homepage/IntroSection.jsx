6f3d0f2cdb216a9eb2a494b3aff740ef3c746c45
LenkaVerzichova <verzichova@brilo.cz>
2022-02-12 14:21:16 +0100

    start intro section

---
+++ components/homepage/IntroSection.jsx
@@ -0,0 +1,11 @@
+import styles from './IntroSection.module.scss';
+
+const IntroSection = () => {
+  return (
+    <>
+      <div className={styles.test}>ahoj</div>
+    </>
+  );
+};
+
+export default IntroSection;
